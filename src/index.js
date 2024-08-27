7a4bc7f3041d233ff8be0ed44f77115951f81d8d
Kam <karadhak@adobe.com>
2024-08-27 21:51:42 +1000

    Headless aem app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './vitals';
+import "@adobe/universal-editor-cors";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals(sendToVercelAnalytics);
