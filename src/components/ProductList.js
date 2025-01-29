fe3c4c54cd306c3a7ad92e179be64050e38d3a26
Kam <kamalesh.radhakrishnan@gmail.com>
2025-01-29 18:03:09 +1100

    Myer Updates

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,25 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import './ProductList.css';
+import React, { useEffect, useState } from "react";
+import "./ProductList.css";
 
 export default function ProductList() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetch('https://publish-p129970-e1316086.adobeaemcloud.com/graphql/execute.json/myer/listproducts')
-            .then(response => response.json())
-            .then(data => setProducts(data.data.productsList.items))
-            .catch(error => console.error('Error fetching products:', error));
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://publish-p129970-e1316086.adobeaemcloud.com/graphql/execute.json/myer/listproducts"
+    )
+      .then((response) => response.json())
+      .then((data) => setProducts(data.data.productsList.items))
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
 
-    return (
-        <div className="product-list">
-            {products.map(product => (
-                <div key={product._path} className="product-tile">
-                    <img  data-aue-filter="cf" data-aue-resource={`urn:aemconnection:${product._path}/jcr:content/data/${product._variation}`} data-aue-prop="productImage" data-aue-type="media" src={`https://publish-p129970-e1316086.adobeaemcloud.com${product.productImage._dynamicUrl}`} alt={product.productTitle} />
-                    <h2  data-aue-filter="cf" data-aue-resource={`urn:aemconnection:${product._path}/jcr:content/data/${product._variation}`} data-aue-prop="productTitle" data-aue-type="text">{product.productTitle}</h2>
-                    <p  data-aue-filter="cf" data-aue-resource={`urn:aemconnection:${product._path}/jcr:content/data/${product._variation}`} data-aue-prop="productDesc" data-aue-type="richtext">{product.productDesc.plaintext}</p>
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className="product-list">
+      {products.map((product) => {
+        const cfConnection = `urn:aemconnection:${product._path}/jcr:content/data/${product._variation}`;
+        return (
+          <div
+            className="section"
+            data-aue-resource={cfConnection}
+            data-aue-type="reference"
+            data-aue-filter="cf"
+          >
+            <div key={product._path} className="product-tile">
+              <img
+                data-aue-prop="productImage"
+                data-aue-type="media"
+                src={`https://publish-p129970-e1316086.adobeaemcloud.com${product.productImage._dynamicUrl}`}
+                alt={product.productTitle}
+              />
+              <h2
+                data-aue-prop="productTitle"
+                data-aue-type="text"
+              >
+                {product.productTitle}
+              </h2>
+              <p
+                data-aue-prop="productDesc"
+                data-aue-type="text"
+              >
+                {product.productDesc.plaintext}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
