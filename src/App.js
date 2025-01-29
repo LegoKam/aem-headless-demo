3dbb1bdc05a98648ea69942e4877215ceda76384
Kam <karadhak@adobe.com>
2025-01-29 11:36:11 +1100

    Myer Updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-import logo from './resources/sportsbet_logo.svg';
-import bell from './resources/bell.svg';
-import avatar from './resources/avatar.png';
-import './App.css';
-import Articles from './components/articles';
-import Accountbalance from './components/accountbalance';
-import Transactions from './components/transactions';
-import Expenses from './components/expenses';
-import CreditCardDetails from './components/creditcarddetails';
-import Footer from './components/footer';
-import FetchContent from './api/contentrequest';
-import FAQ from './components/faq';
+import logo from "./resources/Myer_Logo.svg";
+import bell from "./resources/bell.svg";
+import avatar from "./resources/avatar.png";
+import "./App.css";
+import Articles from "./components/articles";
+import Accountbalance from "./components/accountbalance";
+import Transactions from "./components/transactions";
+import Expenses from "./components/expenses";
+import CreditCardDetails from "./components/creditcarddetails";
+import Footer from "./components/footer";
+import FetchContent from "./api/contentrequest";
+import FAQ from "./components/faq";
 
 function App() {
   const [content, setContent] = useState(null);
@@ -25,58 +26,94 @@ function App() {
     fetchContent();
   }, []);
 
-  const itemId =  "urn:aemconnection:/content/dam/securbank/en/dashboard/account-dashboard/jcr:content/data/master";
-            
-
+  const itemId =
+    "urn:aemconnection:/content/dam/securbank/en/dashboard/account-dashboard/jcr:content/data/master";
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className='header-alerts'>
-            <p><strong>Alert!</strong> Scams are growing ever more complex and sophisticated. Learn more about protecting yourself from scams</p>
-        </div>
-        <div className='header-nav'>
-          <div>
-            <img src={logo} className="logo" alt="logo" />
+    <HelmetProvider>
+      <div className="App">
+        <Helmet>
+          <script
+            src="https://universal-editor-service.experiencecloud.live/corslib/LATEST"
+            async
+          />
+        </Helmet>
+        <header className="App-header">
+          <div className="header-alerts">
+            <p>
+              <strong>Alert!</strong> Scams are growing ever more complex and
+              sophisticated. Learn more about protecting yourself from scams
+            </p>
+          </div>
+          <div className="header-nav">
             <div>
-              <ul>
-                <li><strong>Dashboard</strong></li>
-                <li><a href="#">Bet live</a></li>
-                <li><a href="#">Best Bet Tips</a></li>
-                <li><a href="#">Specials</a></li>
-              </ul>
+              <img src={logo} className="logo" alt="logo" />
+              <div>
+                <ul>
+                  <li>
+                    <strong>Dashboard</strong>
+                  </li>
+                  <li>
+                    <a href="#">Bet live</a>
+                  </li>
+                  <li>
+                    <a href="#">Best Bet Tips</a>
+                  </li>
+                  <li>
+                    <a href="#">Specials</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <img src={bell} className="bell" alt="bell" />
+                <img src={avatar} className="avatar" alt="avatar" />
+                <div className="authFriendly">Kam Radhakrishnan</div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main>
+          <div
+            className="section"
+            data-aue-resource={itemId}
+            data-aue-type="reference"
+            data-aue-filter="cf"
+          >
+            <div>
+              <a href={content && content.bannerUrl}>
+                <img
+                  src={content && content.banner._publishUrl}
+                  className="banner"
+                  alt="banner"
+                  data-aue-prop="banner"
+                  data-aue-type="media"
+                />
+              </a>
+            </div>
+            <div className="twocol">
+              <Accountbalance greeting={content && content.greeting} />
+              <CreditCardDetails cardLabel={content && content.cardLabel} />
+            </div>
+            <div className="twocol">
+              <Transactions
+                transactionTitle={content && content.transactionTitle}
+              />
+              <Expenses expensesTitle={content && content.expensesTitle} />
             </div>
             <div>
-              <img src={bell} className="bell" alt="bell" />
-              <img src={avatar} className="avatar" alt="avatar" />
-              <div className='authFriendly'>Kam Radhakrishnan</div>
+              <FAQ faq={content && content.articles} />
+            </div>
+            <div>
+              <Articles articles={content && content.articles} />
             </div>
           </div>
-      </div>
-        
-      </header>
-      <main >
-        <div className='section' data-aue-resource={itemId} data-aue-type="reference" data-aue-filter="cf">
-          <div><a href={content && content.bannerUrl}><img src={content && content.banner._publishUrl} className="banner" alt="banner" data-aue-prop="banner"  data-aue-type="media"  /></a></div>
-          <div className='twocol'>
-            <Accountbalance greeting={content && content.greeting} />
-            <CreditCardDetails cardLabel={content && content.cardLabel}  />
-          </div>
-          <div className='twocol'>
-            <Transactions transactionTitle={content && content.transactionTitle}/>
-            <Expenses expensesTitle={content && content.expensesTitle} />
-          </div>
-          <div>
-            <FAQ faq={content && content.articles} />
-          </div>
-          <div>
-            <Articles articles={content && content.articles} />
-          </div>
-        </div>
-      </main>
+        </main>
 
-      <footer><Footer /></footer>
-    </div>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </HelmetProvider>
   );
 }
 
