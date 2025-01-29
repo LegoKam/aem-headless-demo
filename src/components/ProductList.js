317dee2bb70c734bff74daeff39e3d5be41f28c2
Kam <kamalesh.radhakrishnan@gmail.com>
2025-01-29 18:34:58 +1100

    Myer Updates

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,9 +11,7 @@ export default function ProductList() {
   }
 
   useEffect(() => {
-    fetch(
-      `${url}`
-    )
+    fetch(url)
       .then((response) => response.json())
       .then((data) => setProducts(data.data.productsList.items))
       .catch((error) => console.error("Error fetching products:", error));
