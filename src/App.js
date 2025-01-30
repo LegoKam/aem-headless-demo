f01439ee2a4cc8f2b52de7b280b7e2f5ca828b31
Kam <kamalesh.radhakrishnan@gmail.com>
2025-01-30 13:50:14 +1100

    myer updates

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import logo from "./resources/Myer_Logo.svg";
 import bell from "./resources/bell.svg";
 import avatar from "./resources/avatar.png";
 import "./App.css";
-// import Articles from "./components/articles";
-import Accountbalance from "./components/accountbalance";
-import Transactions from "./components/transactions";
-import Expenses from "./components/expenses";
-import CreditCardDetails from "./components/creditcarddetails";
+
 import Footer from "./components/footer";
 import FetchContent from "./api/contentrequest";
-import FAQ from "./components/faq";
 import ProductList from "./components/ProductList";
 
 function App() {
