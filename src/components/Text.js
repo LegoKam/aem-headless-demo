00a9b7ee3902a3501f3936719a25de321cce76cf
LegoKam <kamalesh.radhakrishnan@gmail.com>
2025-02-05 10:15:11 +1100

    text changes

--- src/components/Text.js
+++ src/components/Text.js
@@ -5,12 +5,9 @@ const Text = ({ text, content, connectionAndPath }) => {
   return (
     <div 
             data-aue-resource={connectionAndPath}
-    >
-      <div>{text}</div>
-      <div 
             data-aue-type="text"
             data-aue-prop="text"
-            dangerouslySetInnerHTML={{ __html: content }} />
+            dangerouslySetInnerHTML={{ __html: content }} >
     </div>
   );
 };
