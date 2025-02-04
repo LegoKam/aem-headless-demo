7c27676c067b3996e2a55e37b4cfc0392bc1037a
LegoKam <kamalesh.radhakrishnan@gmail.com>
2025-02-04 21:16:58 +1100

    iternate over the nodes

--- src/components/container.js
+++ src/components/container.js
@@ -7,51 +7,27 @@ import Image from './Image';
 const Container = ({ resource, type, isComponent = "" }) => {
   const [components, setComponents] = React.useState(null);
 
-  const createChildComponents = (items, itemid) => {
-    const components = [];
-    for(let key in items) {
-      const item = items[key];
-      const type = item[":type"].split("/").pop();
-      let itemType, Component;
-      
-      switch(type) {
-        case "image": 
-          itemType = "media";
-          Component = Image;
-          break;
-        case "text": 
-          itemType = item.richText ? "richtext" : "text";
-          Component = item.type ? Title : Text;
-          break;
-        case "container": 
-          itemType = "container";
-          Component = Container;
-          break;
-        default: 
-          itemType = "component";
-          Component = () => (<div/>);
-          break;
-      }
-
-      const props = {
-        resource: `${itemid}/${key}`,
-        type: itemType,
-        data: item,
-        isComponent: "component"
-      };
-      components.push(<Component key={key} {...props} />)
-    }
-    return components;
-  }
-
   React.useEffect(() => {
     if(!resource) return;
     fetchData(resource).then((data) => {
       const childNodes = Object.keys(data).filter(key => typeof data[key] === 'object');
-      console.log("CHILD NODES");
-      console.log(childNodes);
+      const childComponents = renderChildComponents(childNodes, data);
+      setComponents(childComponents);
     });
   }, [resource]);
+
+
+  const renderChildComponents = (childNodes, data) => {
+    return childNodes.map((key) => {
+      const childNode = data[key];
+      if (childNode['sling:resourceType'] === 'core/franklin/components/text/v1/text') {
+        return <Text key={key} content={childNode.text} />;
+      } else if (childNode['sling:resourceType'] === 'core/franklin/components/image/v1/image') {
+        return <Image key={key} src={childNode.src} alt={childNode.alt} />;
+      }
+      return null;
+    });
+  };
   
   return (
     <div className="container" data-aue-filter="container-filter" data-aue-model="container" data-aue-behavior={isComponent} data-aue-resource={resource} data-aue-type={type}>
